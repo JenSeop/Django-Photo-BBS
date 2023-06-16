89f15bbd3eec8759fedd5cf8d59ccc24677278f0
JenSeop <dostwoman@gmail.com>
2023-06-16 20:42:41 +0900

    [23.06.16] update photo_list & views & urls

--- phto/urls.py
+++ phto/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .templates.photo import views
+from . import views
 
 urlpatterns = [
     path('', views.photo_list, name='photo_list'),
--- phto/views.py
+++ phto/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Phto
 
 # Create your views here.
-def phto_list(request):
-    return render(request, 'photo/photo_list.html', {})
+# def phto_list(request):
+#     return render(request, 'photo/photo_list.html', {})
+def photo_list(request):
+    photos = Phto.objects.all()
+    return render(request, 'photo/photo_list.html', {'photos': photos})
