83cc46c87d5805496d0896b9677805d2e5655a31
JenSeop <dostwoman@gmail.com>
2023-06-16 12:23:14 +0900

    [23.06.16] update project set

--- myweb/settings.py
+++ myweb/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photo',
 ]
+TIME_ZONE = 'Asia/Seoul'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
