2190626186fa1d76a33052c13803024d7c205034
JenSeop <dostwoman@gmail.com>
2023-06-16 16:53:42 +0900

    [23.06.16] Django model admin page make CRUD

--- phto/models.py
+++ phto/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class phto(models.Model):
+class Phto(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     image = models.CharField(max_length=200)
