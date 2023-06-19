177b5b625af564c837ff7a59afe075be904907a0
JenSeop <dostwoman@gmail.com>
2023-06-19 20:07:47 +0900

    [23.06.19] upload photo_detail module & update

--- phto/views.py
+++ phto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Phto
 
 # Create your views here.
@@ -7,3 +7,7 @@ from .models import Phto
 def photo_list(request):
     photos = Phto.objects.all()
     return render(request, 'photo/photo_list.html', {'photos': photos})
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Phto, pk=pk)
+    return render(request, 'photo/photo_detail.html', {'photo': photo})
